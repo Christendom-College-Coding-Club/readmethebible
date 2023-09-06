1220752bb41bb8543c7354b6be7327a50701505a
joeythornberry <lewis.thornberry@christendom.edu>
2023-09-05 20:13:45 -0400

    add audio testing (it fails for big files)

---
+++ audiotest.py
@@ -0,0 +1,46 @@
+import readmethebible
+import os
+
+def shortSelectionWorks():
+    filename = "Genesis1-1.mp3"
+    log = ""
+    internalLog = [""]
+    shortSelection = readmethebible.get_chapters("bible.txt","Genesis",1,1)
+    readmethebible.read_to_me("Genesis",1,1,shortSelection,200,internalLog)
+    size = os.path.getsize(filename)
+    if size > 1000000:
+        os.remove(filename)
+        return True
+    else:
+        log += "internalLog: " + internalLog[0] + "\n"
+        log += "size: " + str(size) + "\n"
+        log += "shortSelectionWorks failed\n"
+        os.remove(filename)
+        print(log)
+        return False
+
+
+def longSelectionWorks():
+    filename = "Genesis1-50.mp3"
+    log = ""
+    internalLog = [""]
+    longSelection = readmethebible.get_chapters("bible.txt","Genesis",1,50)
+    readmethebible.read_to_me("Genesis",1,50,longSelection,200,internalLog)
+    size = os.path.getsize(filename)
+    if size > 1000000:
+        os.remove(filename)
+        return True
+    else:
+        log += "internalLog: " + internalLog[0] + "\n"
+        log += "size: " + str(size) + "\n"
+        log += "longSelectionWorks failed\n"
+        os.remove(filename)
+        print(log)
+        return False
+
+
+print("\n\n")
+if shortSelectionWorks():
+    print("shortSelectionWorks passed", end="\n\n")
+if longSelectionWorks():
+    print("longSelectionWorks passed", end="\n\n")
--- readmethebible.py
+++ readmethebible.py
@@ -1,4 +1,5 @@
 import pyttsx3
+import sys
 
 def get_first_number(line):
     first_number = ""
@@ -12,7 +13,7 @@ def get_first_number(line):
             break
     return -1 if first_number == "" else first_number
 
-def get_chapters(bible, book, first, last):
+def get_chapters(bible, book, first, last, log = ""):
     
     looking_for_book = 1
     looking_for_chapter = 2
@@ -34,22 +35,24 @@ def get_chapters(bible, book, first, last):
                 first_number = get_first_number(line)
                 if int(first_number) == first:
                     state = reading_chapter
-                    print("found ",first_number)
+                    log += "found chapter " + str(first) + "\n"
                 
             if state == reading_chapter:
                 first_number = get_first_number(line)
                 if int(first_number) > last:
-                    print("found ",first_number)
+                    log += "finished reading chapter " + str(first) + "\n"
                     break
                 else:
                     numberless_line = ''.join([i for i in line if not i.isdigit() and not i == ':'])
                     if numberless_line:
                         to_read += numberless_line
                         to_read += " "
+
+    log += to_read + "\n"
     
     return to_read
 
-def read_to_me(book,start,end,to_read,speed):  
+def read_to_me(book,start,end,to_read,speed,log = [""]):  
     try:
         engine = pyttsx3.init()
         
@@ -57,18 +60,17 @@ def read_to_me(book,start,end,to_read,speed):
         engine.setProperty('volume', 1)
         
         try:
-            print("saving to file, will close when done")
-            print("to read: ",to_read)
+            log[0] += "saving to file, will close when done" + "\n"
             engine.save_to_file(to_read,f"{book}{start}-{end}.mp3")
         except Exception as e:
-            print(e)
-            print("error saving to file. exiting...")
+            log[0] += str(e) + "\n"
+            log[0] += "error saving to file. exiting..." + "\n"
         
         engine.runAndWait()
-        print("save completed")
+        log[0] += "save completed" + "\n"
     except Exception as e:
-        print(e)
-        print("Error initializing pyttsx3. Exiting...")
+        log[0] += str(e) + "\n"
+        log[0] += "error initializing engine. exiting..." + "\n"
 
 def read_me_the_bible(bible,book,first,last,speed):
     print("gathering selection...")
@@ -76,11 +78,24 @@ def read_me_the_bible(bible,book,first,last,speed):
     print("read successful")
     read_to_me(book,first,last,to_read,speed)
 
-print("Ctrl+c to Exit")
-book = input("Book: ").strip()
-book = book.lower().capitalize()
-first = int(input("First Chapter: "))
-last = int(input("Last Chapter: "))
-print("150-200 is a good speed")
-speed = int(input("Speed: "))
-read_me_the_bible("bible.txt",book,first,last,speed)
+if __name__ == "__main__":
+    if len(sys.argv) == 5:
+        book = sys.argv[1].strip().lower().capitalize()
+        first = int(sys.argv[2])
+        last = int(sys.argv[3])
+        speed = int(sys.argv[4])
+    elif len(sys.argv) == 1:
+        print("Ctrl+c to Exit")
+        book = input("Book: ").strip()
+        book = book.lower().capitalize()
+        first = int(input("First Chapter: "))
+        last = int(input("Last Chapter: "))
+        print("150-200 is a good speed")
+        speed = int(input("Speed: "))
+    else:
+        print("Usage: readmethebible or readmethebible <book> <first chapter> <last chapter> <speed>")
+        print("Example: python3 bible_reader.py Genesis 1 50 150")
+        print("Example: python3 bible_reader.py")
+        sys.exit(1)
+    
+    read_me_the_bible("bible.txt",book,first,last,speed)
